0465ded363abe95a5061f6ee372110f2a1cddf9b
EmmanuelGuardado <emmanuelgangel@gmail.com>
2020-06-19 01:47:09 -0500

    Agregando carpeta 'Modelos' y ejecutando scaffold de la base de datos

---
+++ RecordatorioContraseñas/Modelos/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordatorioContraseñas.Modelos
+{
+    public partial class Usuarios
+    {
+        public Usuarios()
+        {
+            Cuentas = new HashSet<Cuentas>();
+        }
+
+        public byte Id { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool? Activo { get; set; }
+
+        public virtual ICollection<Cuentas> Cuentas { get; set; }
+    }
+}
